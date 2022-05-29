1858a71d74df31bdda7c7d71287150d65543b725
abuiliazeed <abuiliazeed@gmail.com>
2022-05-29 15:43:57 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    <div className="bg-gray-50 text-4xl flex flex-col p-10 items-center">
+        Getting Started
+      </div>
+      <div>
+        <ul className="flex flex-row p-5">
+          <li className="p-5 shadow-sm rounded-sm m-2 w-full text-center text-xl font-bold">First Card</li>
+          <li className="p-5 shadow-sm rounded-sm m-2 w-full text-center text-xl font-bold">Second Card</li>
+          <li className="p-5 shadow-sm rounded-sm m-2 w-full text-center text-xl font-bold">Third Card</li>
+          <li className="p-5 shadow-sm rounded-sm m-2 w-full text-center text-xl font-bold">Fourth Card</li>
+          <li className="p-5 shadow-sm rounded-sm m-2 w-full text-center text-xl font-bold">Fifth Card</li>
+
+        </ul>
+      </div>
+    </>
+    
   );
 }
 
