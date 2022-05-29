427605c3f899ecf4054f191cf72a8f7ab701648d
abuiliazeed <abuiliazeed@gmail.com>
2022-05-29 15:59:15 +0200

    adding bg

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-
+import Businesscard from "./components/businesscard";
 
 function App() {
   return (
     <> 
-    <div className="bg-gray-50 text-4xl flex flex-col p-10 items-center">
-        Getting Started
-      </div>
-      <div>
-        <ul className="flex flex-row p-5">
-          <li className="p-5 shadow-sm rounded-sm m-2 w-full text-center text-xl font-bold">First Card</li>
-          <li className="p-5 shadow-sm rounded-sm m-2 w-full text-center text-xl font-bold">Second Card</li>
-          <li className="p-5 shadow-sm rounded-sm m-2 w-full text-center text-xl font-bold">Third Card</li>
-          <li className="p-5 shadow-sm rounded-sm m-2 w-full text-center text-xl font-bold">Fourth Card</li>
-          <li className="p-5 shadow-sm rounded-sm m-2 w-full text-center text-xl font-bold">Fifth Card</li>
-
-        </ul>
-      </div>
+    <Businesscard />
     </>
     
   );
---
+++ src/components/businesscard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Businesscard() {
+  return (
+    <>
+    <div className='flex flex-row items-center justify-center'>
+    <div className='text-white w-[550px] h-[868px] bg-[#23252C]'>
+test
+        </div>
+    </div>
+        
+    </>
+  )
+}
+
+export default Businesscard
