3f4919ac34bf8e8ede8e9555df8655d603895a3f
abuiliazeed <abuiliazeed@gmail.com>
2022-05-29 16:04:21 +0200

    update

--- src/components/businesscard.jsx
+++ src/components/businesscard.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Businesscard() {
   return (
     <>
-    <div className='flex flex-row items-center justify-center'>
-    <div className='text-white w-[550px] h-[868px] bg-[#23252C]'>
-test
+      <div className="flex flex-row items-center justify-center">
+        <div className="text-whitehhhhhh w-[550px] h-[868px] bg-[#23252C]">
+         <div className="flex flex-col items-center justify-center p-16" >
+
+             <div className="w-[317px] h-[399px] bg-[#1A1B21]">
+
+             </div>
+
+         </div>
         </div>
-    </div>
-        
+      </div>
     </>
-  )
+  );
 }
 
-export default Businesscard
+export default Businesscard;
