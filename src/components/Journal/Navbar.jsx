491869dc962de17d1aa80c67158b0bc9be8606b5
abuiliazeed <abuiliazeed@gmail.com>
2022-05-30 16:24:08 +0200

    update

--- src/components/Journal/Navbar.jsx
+++ src/components/Journal/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {BiWorld} from 'react-icons/bi'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+      <>
+          <div className='p-4 flex flex-row items-center justify-center bg-[#F55A5A] text-white'>
+        <BiWorld className='text-4xl'/>
+        <p className='text-2xl pl-2'>my travel journal.</p>
+        </div>
+      
+      </>
+
   )
 }
 
