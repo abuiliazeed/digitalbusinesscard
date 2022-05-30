4557d31721c182c3bb197def8e76f4df0944b11e
abuiliazeed <abuiliazeed@gmail.com>
2022-05-30 16:05:40 +0200

    update

---
+++ src/Journal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Journals from './components/Journal/Journals'
+import Navbar from './components/Journal/Navbar'
+
+function Journal() {
+  return (
+    <>
+    <Navbar />
+    <Journals />
+    
+    </>
+  )
+}
+
+export default Journal
---
+++ src/data/journaldata.js
@@ -0,0 +1,35 @@
+const journalData = [
+    {
+        id: 1,
+        image: "./images/fuji.png",
+        country:"Japan",
+        geolink:"https://googlemaps.com",
+        title:"Mount Fuji",
+        startdate:"2020-01-01",
+        enddate:"2020-01-01",
+        description:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        id: 2,
+        image: "./images/sydny.png",
+        country:"sydny",
+        geolink:"https://googlemaps.com",
+        title:"Sydny Opera House",
+        startdate:"2020-01-01",
+        enddate:"2020-01-01",
+        description:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        id: 3,
+        image: "./images/norway.png",
+        country:"norway",
+        geolink:"https://googlemaps.com",
+        title:"Mount Fuji",
+        startdate:"2020-01-01",
+        enddate:"2020-01-01",
+        description:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    }
+
+]
+
+export default journalData
