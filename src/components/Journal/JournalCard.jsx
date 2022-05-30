8b0a94d2a36b31deacdf98e71e2bdbaa57fa3eb2
abuiliazeed <abuiliazeed@gmail.com>
2022-05-30 16:38:18 +0200

    update

---
+++ src/components/Journal/JournalCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function JournalCard() {
+  return (
+    <>
+        <div className='flex flex-row  border-b'>
+            <div className='p-4'>image</div>
+            <div className='p-4'>data</div>
+        </div>
+        <div className=''></div>
+    </>
+  )
+}
+
+export default JournalCard
--- src/components/Journal/Journals.jsx
+++ src/components/Journal/Journals.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import journalData from '../../data/journaldata'
+import JournalCard from './JournalCard'
+
+const journalcards = journalData.map(item =>{
+    return <JournalCard key={item.id} {...item}/>
+})
 
 function Journals() {
   return (
-    <div>Journals</div>
+    <>
+{journalcards}
+
+    
+    </>
   )
 }
 
