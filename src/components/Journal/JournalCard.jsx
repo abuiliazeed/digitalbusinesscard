1622c8ef70890fab18bac03bd10513b91225d8e0
abuiliazeed <abuiliazeed@gmail.com>
2022-05-31 15:22:06 +0200

    update

--- src/components/Journal/JournalCard.jsx
+++ src/components/Journal/JournalCard.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
 
-function JournalCard() {
+function JournalCard(props) {
   return (
     <>
-        <div className='flex flex-row  border-b'>
-            <div className='p-4'>image</div>
-            <div className='p-4'>data</div>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-row  border-gray-100 border-b max-w-[75%] ">
+          <div className="px-4 py-4 min-w-max">
+            <img src={props.image} alt="" />
+          </div>
+          <div className="flex flex-col p-4 items-start">
+              <div className="">
+                  <span>{props.country}</span><span>{props.geolink}</span>
+                  </div>
+              <div>{props.title}</div>
+              <div>
+                  <span>{props.startdate}</span>-<span>{props.enddate}</span>
+              </div>
+              <div>{props.description}</div>
+          </div>
         </div>
-        <div className=''></div>
+      </div>
+
+   
+
     </>
-  )
+  );
 }
 
-export default JournalCard
+export default JournalCard;
