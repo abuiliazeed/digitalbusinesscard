907cef4d784dcc03185e050246674620fd78abad
abuiliazeed <abuiliazeed@gmail.com>
2022-06-26 16:56:37 +0200

    update

--- src/components/Excercises/Yes.jsx
+++ src/components/Excercises/Yes.jsx
@@ -5,6 +5,20 @@ function Yes() {
    const handleClick = () => {
     if (result === "Yes") {setResult("No")}else {setResult("Yes")}
 } 
+
+const [counter, setCounter] = useState(0);
+const handleCountup = () => {
+    setCounter(counter + 1);
+}
+const handleCountdown = () => {
+    setCounter(counter - 1);
+}
+const handleReset = () => {
+    setCounter(0);
+}
+
+
+
   return (
     <>
       <div className="bg-black flex flex-col justify-center items-center p-4">
@@ -12,6 +26,21 @@ function Yes() {
         <div onClick={handleClick} className="rounded-full bg-white w-60 h-60 flex items-center justify-center m-8 hover:scale-105 hover:cursor-pointer">
           <h1 className="text-[90px] font-bold text-center">{result}</h1>
         </div>
+
+        <div className="flex items-baseline justify-center ">
+        <div onClick={handleCountdown} className="rounded-full bg-white w-20 h-20 flex items-center justify-center hover:scale-105 hover:cursor-pointer">
+          <h1 className="text-[60px] font-bold text-center">-</h1>
+        </div>
+        <div onClick={handleReset} className="rounded-full bg-white w-60 h-60 flex items-center justify-center  hover:scale-105 hover:cursor-pointer">
+          <h1 className="text-[90px] font-bold text-center">{counter}</h1>
+        </div>
+        <div onClick={handleCountup} className="rounded-full bg-white w-20 h-20 flex items-center justify-center  hover:scale-105 hover:cursor-pointer">
+          <h1 className="text-[60px] font-bold text-center">+</h1>
+        </div>
+
+        </div>
+        
+
       </div>
     </>
   );
