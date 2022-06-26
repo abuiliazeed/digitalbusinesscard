c25029e1d2f0f2fe94b18e29dcd830ebc7ba594f
abuiliazeed <abuiliazeed@gmail.com>
2022-06-26 17:08:10 +0200

    update

--- src/components/Excercises/Yes.jsx
+++ src/components/Excercises/Yes.jsx
@@ -28,14 +28,14 @@ const handleReset = () => {
         </div>
 
         <div className="flex items-baseline justify-center ">
-        <div onClick={handleCountdown} className="rounded-full bg-white w-20 h-20 flex items-center justify-center hover:scale-105 hover:cursor-pointer">
-          <h1 className="text-[60px] font-bold text-center">-</h1>
+        <div onClick={handleCountdown} className="rounded-full bg-gray-500 w-20 h-20 flex items-center justify-center hover:scale-105 hover:cursor-pointer">
+          <h1 className="text-[60px] font-bold text-center text-white">-</h1>
         </div>
         <div onClick={handleReset} className="rounded-full bg-white w-60 h-60 flex items-center justify-center  hover:scale-105 hover:cursor-pointer">
           <h1 className="text-[90px] font-bold text-center">{counter}</h1>
         </div>
-        <div onClick={handleCountup} className="rounded-full bg-white w-20 h-20 flex items-center justify-center  hover:scale-105 hover:cursor-pointer">
-          <h1 className="text-[60px] font-bold text-center">+</h1>
+        <div onClick={handleCountup} className="rounded-full bg-gray-500 w-20 h-20 flex items-center justify-center  hover:scale-105 hover:cursor-pointer">
+          <h1 className="text-[60px] font-bold text-center text-white">+</h1>
         </div>
 
         </div>
